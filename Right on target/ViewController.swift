59877a3c61c9f989bfd4eb1523b85a524bcdb6ea
Gleb <monachusgl@gmail.com>
2022-01-30 23:47:59 +0300

    Добавлены основные методы для ViewController

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -13,17 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet var label: UILabel!
     
     var number: Int = 0
-    var round: Int = 0
+    var round: Int = 1
     var points: Int = 0
     
     @IBAction func checkNumber() {
         
         switch self.round {
-        case 0:
-            self.number = Int.random(in: 1...50)
-            self.label.text = String(self.number)
-            self.round = 1
-            
+        /*case 0:
+         self.number = Int.random(in: 1...50)
+         self.label.text = String(self.number)
+         self.round = 1*/
+        
         case 1...4:
             let numSlider = Int(slider.value.rounded())
             switch numSlider {
@@ -33,10 +33,10 @@ class ViewController: UIViewController {
                 
             case _ where numSlider < self.number:
                 self.points += 50 - self.number + numSlider
-            
+                
             case _ where numSlider == self.number:
-            self.points += 50
-                          
+                self.points += 50
+                
             default:
                 return
             }
@@ -48,7 +48,7 @@ class ViewController: UIViewController {
             let alert = UIAlertController(
                 title: "Игра окончена",
                 message: "Вы заработали \(self.points) очков",
-                	preferredStyle: .alert)
+                preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
             self.label.text = "Последний результат \(self.points)"
@@ -59,14 +59,32 @@ class ViewController: UIViewController {
         }
         
     }
-    
-    
-    
+    override func loadView() {
+        super.loadView()
+        print("loadView")
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+        self.number = Int.random(in: 1...50)
+        self.label.text = String(self.number)
     }
-    
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisapper")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
 }
 
